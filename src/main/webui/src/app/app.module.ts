edf5b114d43dbb53738aeb45e863c2baf5be7450
David Schwingenschlögl <david.schwingenschloegl@dashumankapital.net>
2023-04-12 22:18:52 +0200

    Bumped versions in webui

--- src/main/webui/src/app/app.module.ts
+++ src/main/webui/src/app/app.module.ts
@@ -40,7 +40,7 @@ import {MatProgressBarModule} from "@angular/material/progress-bar";
 import {MatIconModule} from "@angular/material/icon";
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {SpinnerComponent} from './spinner/spinner.component';
-import {FlexLayoutModule, FlexModule} from "@angular/flex-layout";
+import {FlexLayoutModule} from "@angular/flex-layout";
 import {MatDividerModule} from "@angular/material/divider";
 
 export function httpTranslateLoaderFactory(http: HttpClient) {
@@ -76,6 +76,7 @@ export function httpTranslateLoaderFactory(http: HttpClient) {
     MatToolbarModule,
     MatButtonModule,
     FormsModule,
+    ReactiveFormsModule,
     FlexLayoutModule,
     MatCheckboxModule,
     MatCardModule,
